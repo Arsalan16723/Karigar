ca28ba99d7a93f63f80ca3c8847c176c82f0fa10
Arsalan16723 <m.arsalan16723@gmail.com>
2016-06-27 12:06:37 +0500

    added errors partials

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,3 +1,9 @@
 class Worker < ActiveRecord::Base
 	belongs_to :category
+	validates :name, presence: true, length: {minimum: 4, maximum: 50}
+	validates :cnic, presence: true, length: {minimum: 13, maximum: 13}
+	validates :phone, presence: true, length: {minimum: 11, maximum: 13}
+	validates :city, presence: true, length: {minimum: 3, maximum: 25}
+	validates :category_id, presence: true
 end
+1620255616031
